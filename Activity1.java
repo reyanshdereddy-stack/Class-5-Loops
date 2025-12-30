0047f26dd1104b5b3f5752a3a156bae1f6f2d9e6
reyanshdereddy-stack <reyanshdereddy@gmail.com>
2025-12-30 18:29:40 -0500

    v1

---
+++ Activity1.java
@@ -0,0 +1,9 @@
+class Activity1{
+    public static void main(String[] args) {
+        int i;
+        for (i=10; i>1; i--){
+            System.out.println("The value of i is "+i);
+        }
+    }
+}
+
